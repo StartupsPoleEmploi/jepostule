510ffb7c807c2eb18f252e976358a3c2b4a8bca4
Régis Behmo <regis@behmo.com>
2018-11-12 13:20:58 +0100

    Serve static assets from django

--- config/urls.py
+++ config/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic.base import RedirectView
@@ -9,3 +11,6 @@ urlpatterns = [
     path('', include(('jepostule.pipeline.urls', 'pipeline'))),
     path('admin/', admin.site.urls),
 ]
+
+# For now, we serve static assets from django. Yes, we know it's bad.
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
