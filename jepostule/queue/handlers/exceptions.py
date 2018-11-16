3ce88fc4ca37b323d0f753546e0d11bdd73ea3d4
Régis Behmo <regis@behmo.com>
2018-11-16 09:34:05 +0100

    Fix failed applications with large attachments
    Default max_request_size was 1Mb, which was throwing away applications.
    We now raise errors because we check the result of send ().

---
+++ jepostule/queue/handlers/exceptions.py
@@ -0,0 +1,2 @@
+class ProduceError(Exception):
+    pass
--- jepostule/queue/handlers/franz.py
+++ jepostule/queue/handlers/franz.py
@@ -4,6 +4,7 @@ from django.conf import settings
 import kafka
 
 from . import base
+from . import exceptions
 
 
 logger = logging.getLogger(__name__)
@@ -14,15 +15,19 @@ class KafkaProducer(base.BaseProducer):
     def send(self, topic, value, key=None):
         producer = kafka.KafkaProducer(
             bootstrap_servers=settings.KAFKA_BOOTSTRAP_SERVERS,
+            max_request_size=11534336,
         )
-        producer.send(
+        result = producer.send(
             topic, value=value, key=key,
         ).add_errback(self.on_send_error)
+        if result.failed():
+            raise exceptions.ProduceError(topic, value, key)
         producer.flush()
 
     @staticmethod
     def on_send_error(e):
-        raise e
+        # Don't raise an exception here, because it's going to be caught
+        logger.exception(e)
 
 
 class KafkaConsumer(base.BaseConsumer):
