8d9de73e5d70d0e34e6e7cc3e83ea19a20b27dd1
kemar <marc@marcarea.com>
2019-05-16 16:31:13 +0200

    Remove `useLocalStorage` from global scope

--- jepostule/static/js/common/localStorage.js
+++ jepostule/static/js/common/localStorage.js
@@ -2,34 +2,31 @@
 
 document.addEventListener('DOMContentLoaded', function () {
 
+  let useLocalStorage = inputs => {
+
+    if (!inputs.length) {
+      return
+    }
+
+    let saveToLocalStorage = (e) => {
+      let input = e.target
+      if (input.value) {
+        localStorage.setItem(input.name, input.value)
+      }
+    }
+
+    // Load from local storage.
+    Array.from(inputs).forEach(input => {
+      let value = localStorage.getItem(input.name)
+      if (value) {
+        input.value = value
+        input.dispatchEvent(new Event('change'), {'bubbles': true})
+      }
+      input.addEventListener('blur', saveToLocalStorage)
+    })
+
+  }
+
   useLocalStorage(document.querySelectorAll('[data-localstorage]'))
 
 })
-
-let useLocalStorage = inputs => {
-
-  if (!inputs.length) {
-    return
-  }
-
-  // Load from local storage.
-  Array.from(inputs).forEach(input => {
-    let value = localStorage.getItem(input.name)
-    if (value) {
-      input.value = value
-      input.dispatchEvent(new Event('change'), {'bubbles': true})
-    }
-  })
-
-  let saveToLocalStorage = (e) => {
-    let input = e.target
-    if (input.value) {
-      localStorage.setItem(input.name, input.value)
-    }
-  }
-
-  Array.from(inputs).forEach(el => {
-    el.addEventListener('blur', saveToLocalStorage)
-  })
-
-}
